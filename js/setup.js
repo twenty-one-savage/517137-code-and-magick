8f8c7c5917b6e56c3d2e5017f4b64e1b9614c61c
twenty-one-savage <Muravtsev21@gmail.com>
2018-11-26 16:12:40 +0300

    Исправил замечания, но не уверен, что правильно

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,7 @@ var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 // В найденном окне настроек находим поле с похожими волшебниками, позже мы будем добавлять в этот список волшебников
-var similarListElement = document.querySelector('.setup-similar-list');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
 // Обращаемся к шаблону и его содержимому, который в дальнейшем будем клонировать (через cloneNode)
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
@@ -20,52 +20,42 @@ var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var getRandomInt = function (max, min) {
   return Math.floor(Math.random() * (max - min) + min);
 };
-// Массив со свойствамим волшебника(имя, фамилия, цвет мантии, цвет глаз)
-var wizards = [
-  {
-    name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length, 0)],
-    surname: WIZARDS_SURNAMES[getRandomInt(WIZARDS_SURNAMES.length, 0)],
-    coatColor: WIZARD_COAT_COLORS[getRandomInt(WIZARD_COAT_COLORS.length, 0)],
-    eyesColor: WIZARD_EYES_COLORS[getRandomInt(WIZARD_EYES_COLORS.length, 0)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length, 0)],
-    surname: WIZARDS_SURNAMES[getRandomInt(WIZARDS_SURNAMES.length, 0)],
-    coatColor: WIZARD_COAT_COLORS[getRandomInt(WIZARD_COAT_COLORS.length, 0)],
-    eyesColor: WIZARD_EYES_COLORS[getRandomInt(WIZARD_EYES_COLORS.length, 0)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length, 0)],
-    surname: WIZARDS_SURNAMES[getRandomInt(WIZARDS_SURNAMES.length, 0)],
-    coatColor: WIZARD_COAT_COLORS[getRandomInt(WIZARD_COAT_COLORS.length, 0)],
-    eyesColor: WIZARD_EYES_COLORS[getRandomInt(WIZARD_EYES_COLORS.length, 0)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length, 0)],
-    surname: WIZARDS_SURNAMES[getRandomInt(WIZARDS_SURNAMES.length, 0)],
-    coatColor: WIZARD_COAT_COLORS[getRandomInt(WIZARD_COAT_COLORS.length, 0)],
-    eyesColor: WIZARD_EYES_COLORS[getRandomInt(WIZARD_EYES_COLORS.length, 0)]
-  }
-];
 
-// Функция для отрисовки волшебника
+// Массив с количеством волшебников
+var wizards = [{}, {}, {}, {}];
+
+// Создаем свойства для волшебников (имя, цвет мантии, цвет глаз)
+for (var i = 0; i < wizards.length; i++) {
+  wizards[i].name = WIZARDS_SURNAMES[getRandomInt(WIZARDS_SURNAMES.length, 0)] + WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length, 0)];
+  wizards[i].coatColor = WIZARD_COAT_COLORS[getRandomInt(WIZARD_COAT_COLORS.length, 0)];
+  wizards[i].eyesColor = WIZARD_EYES_COLORS[getRandomInt(WIZARD_EYES_COLORS.length, 0)];
+}
+
+// Функция для создания волшебника
 var renderWizard = function (wizard) {
   // Клонируем содержимое тега <template> и записываем в переменную
   var wizardElement = similarWizardTemplate.cloneNode(true);
   // Далее работаем со свойствами объекта (имя и фамилия, цвет мантии, цвет глаз)
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.surname + wizard.name;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
 };
-// Создаем пустой фрагмент
-var fragment = document.createDocumentFragment();
-// Далее вставляем в ранее созданный фрагмент волшебников
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-// Вставляем фрагмент в DOM-дерево
-similarListElement.appendChild(fragment);
+
+// Функция для отрисовки волшебника на странице
+var drawWizards = function (arr) {
+  // Создаем пустой фрагмент
+  var fragment = document.createDocumentFragment();
+  // Далее вставляем, в ранее созданный фрагмент, волшебников
+  for (i = 0; i < arr.length; i++) {
+    fragment.appendChild(renderWizard(arr[i]));
+  }
+  return fragment;
+};
+
+// Вставляем содержимое фрагмента в DOM-дерево
+similarListElement.appendChild(drawWizards(wizards));
+
 // Отображаем список похожих магов на странице
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
